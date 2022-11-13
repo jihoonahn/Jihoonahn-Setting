d6018fa6953e7b0573401d81ff2fbee3fdc541a2
안지훈 <ahnjh1028@naver.com>
2022-11-14 00:21:37 +0900

    Update install.sh

--- vscode/install.sh
+++ vscode/install.sh
@@ -6,4 +6,4 @@ sudo xattr -dr com.apple.quarantine /Applications/Visual\ Studio\ Code.app
 
 # install vscode extensions
 code --install-extensions MS-CEINTL.vscode-language-pack-ko
-code -- install-extensions bierner.markdown-preview-github-styles
+code --install-extensions bierner.markdown-preview-github-styles
